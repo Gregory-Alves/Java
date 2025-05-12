bd0deeef14c9437c9986d0d9466cb9d993176e46
Gregory-Alves <gnajr2003@gmail.com>
2025-05-12 15:40:23 -0300

    Interfaces

---
+++ CURSOS/JavaBasico/src/com/gregory/cursojava/aula44/AnimalDomesticado.java
@@ -0,0 +1,8 @@
+package com.gregory.cursojava.aula44;
+
+public interface AnimalDomesticado {
+
+    void levarVeterinario();
+    void alimentar();
+
+}
---
+++ CURSOS/JavaBasico/src/com/gregory/cursojava/aula44/AnimalEstimacao.java
@@ -0,0 +1,8 @@
+package com.gregory.cursojava.aula44;
+
+public interface AnimalEstimacao {
+
+    void brincar ();
+    void levarPassear();
+
+}
